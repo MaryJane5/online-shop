e208d43deebf8a44e1a9ba60233a92b9f7945c6a
SimplyMaxx <maksimba05@gmail.com>
2019-09-04 19:34:16 +0300

    tabs

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,10 @@
-$(document).ready(function() {
-
-    
-
+//tabs
+$('.tab').click(function (event) {
+    $(".chosen").removeClass("chosen");
+    $(this).addClass('chosen');
+    //get index of tab
+    var index = $('.tab').index($(this));
+    //go to the page[index]
+    $(".shown").removeClass("shown");
+    $('.page').eq(index).addClass('shown');
 });
