91c9071424a8e152b0a99bbd17fd2abd53c26891
dykchinkhoanh <dykchinkhoanh93@gmail.com>
2019-09-10 09:16:11 +0300

    added tabs

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,15 @@
+//tabs
+$('.tab').click(function (event) {
+    $(".chosen").removeClass("chosen");
+    $(this).addClass('chosen');
+    //get index of tab
+    var index = $('.tab').index($(this));
+    //go to the page[index]
+    $(".shown").removeClass("shown");
+    $('.page').eq(index).addClass('shown');
+
+});
+
 $(document).ready(function() {
 
     $(".btn-show-profile-info").click(function() {
